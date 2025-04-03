5ad33381fe99490f1c96cbf8366004a4a5d90695
潘卓然 <21371249@buaa.edu.cn>
2025-04-03 14:37:08 +0800

    submit rank algorithm and data process

---
+++ data_process/make_domain_list_json.py
@@ -0,0 +1,13 @@
+import json
+
+# 读取域名
+with open('../domain_list/use_this_blacklist.txt', 'r') as f:
+    domains = [line.strip() for line in f]
+
+# 创建包含 fqdn 和 value 的字典列表
+domain_dicts = [{'fqdn': domain, 'value': 1} for domain in domains]
+
+# 将字典列表写入到 JSON 文件中
+with open('../domain_list/use_this_blacklist.json', 'w') as f:
+    for domain_dict in domain_dicts:
+        f.write(json.dumps(domain_dict) + '\n')
